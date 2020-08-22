e1aae43737540a6cfc01993b10a0a5644ae8acd0
Danny Shen <shenfive@gmail.com>
2020-08-22 17:21:09 +0800

    fin

--- a207API/ViewController.swift
+++ a207API/ViewController.swift
@@ -12,6 +12,7 @@ import Kingfisher
 
 class ViewController: UIViewController {
     @IBOutlet weak var bigHade: UIImageView!
+    @IBOutlet weak var ind: UIActivityIndicatorView!
     
     @IBOutlet weak var emailL: UILabel!
     @IBOutlet weak var telL: UILabel!
@@ -20,16 +21,34 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         updateUser()
  
+
+ 
+        
         
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        bigHade.clipsToBounds = true
+        bigHade.layer.cornerRadius = bigHade.bounds.size.height / 2
+        bigHade.layer.borderColor = UIColor.black.cgColor
+        bigHade.layer.borderWidth = 3
+        
+        
+    }
+    
 
     @IBAction func nextUser(_ sender: Any) {
         updateUser()
     }
     
     func updateUser(){
+        self.ind.isHidden = false
+        self.ind.startAnimating()
         let model = APIModel.share
         model.queryRandomUser { (respons, error) in
+            self.ind.isHidden = true
+            self.ind.startAnimating()
             let json = JSON(respons as! Data)
             let imageString  = json["results"].arrayValue[0]["picture"]["large"].stringValue
             if let url = URL(string: imageString){
